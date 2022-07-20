6400d4687b43148a633852fd773ad733a50df9c5
JQ <hi.jiaqizhang@gmail.com>
2022-07-20 22:56:36 +0100

    Update [tokenIds].js

--- my-app/pages/api/[tokenIds].js
+++ my-app/pages/api/[tokenIds].js
@@ -2,8 +2,11 @@ export default function handler(req, res) {
   // get the tokenId from the query params
   const tokenId = req.query.tokenId;
   // As all the images are uploaded on github, we can extract the images from github directly.
+  // const image_url =
+  //   "https://ipfs.io/ipfs/QmNU12GQ59rnZkdpkZHt7DoFFQv76rkGSPSYRLxWinSkaU";
   const image_url =
-    "https://ipfs.io/ipfs/QmNU12GQ59rnZkdpkZHt7DoFFQv76rkGSPSYRLxWinSkaU";
+  "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/";
+  
   // The api is sending back metadata for a Crypto Dev
   // To make our collection compatible with Opensea, we need to follow some Metadata standards
   // when sending back the response from the api
