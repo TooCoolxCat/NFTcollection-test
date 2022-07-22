256959fad39601409330ca13a5b0b7c613bf4a45
JQ <hi.jiaqizhang@gmail.com>
2022-07-22 17:22:00 +0100

    update

--- hardhat-tutorial/scripts/deploy.js
+++ hardhat-tutorial/scripts/deploy.js
@@ -1,5 +1,4 @@
 const { ethers } = require("hardhat");
-require("dotenv").config({ path: ".env" });
 const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require("../constants");
 
 async function main() {
